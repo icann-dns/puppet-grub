e02394a2989c82af5bf596afe9e070179e320eef
jbond <github@johnbond.org>
2024-07-02 13:48:25 +0200

    init: add support for providing a custom grub content
    Bug: DNSIN-216

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,29 +1,45 @@
 # == Class: grub
 #
+# @param protect_boot protect boot
+# @param protect_advanced protect advanced options
+# @param user the username to user
+# @param the password to use
 class grub (
-  Optional[String] $user             = undef,
-  Optional[String] $password         = undef,
-  Boolean          $protect_boot     = false,
-  Boolean          $protect_advanced = false,
+  Boolean             $protect_boot     = false,
+  Boolean             $protect_advanced = false,
+  Optional[String]    $user             = undef,
+  Optional[String]    $password         = undef,
+  Optional[String[1]] $custom_content   = undef,
 ) {
+  if $custom_source and $custom_content {
+    fail('you can only provide one of `custom_source` or `custom_content`')
+  }
+  $_custom_content = @("CUSTOM"\L)
+  #!/bin/sh
+  exec tail -n +3 $0
+  ${custom_content},
+  | CUSTOM
+
   exec {'update_grub':
     command     => '/usr/sbin/update-grub',
     refreshonly => true,
   }
-  file {'/etc/default/grub':
-    ensure  => present,
-    content => template('grub/etc/default/grub.erb'),
-    notify  => Exec['update_grub'],
-  }
-  file {'/etc/grub.d/10_linux':
-    ensure  => present,
-    mode    => '0755',
-    content => template('grub/etc/grub.d/10_linux.erb'),
-    notify  => Exec['update_grub'],
+  file {
+    default:
+      notify  => Exec['update_grub'],
+      mode   => '0755',
+      ensure  => file;
+    '/etc/default/grub':
+      mode   => '0644',
+      content => template('grub/etc/default/grub.erb');
+    '/etc/grub.d/10_linux':
+      content => template('grub/etc/grub.d/10_linux.erb'),
+    '/etc/grub.d/40_custom':
+      content => $_custom_content,
   }
   if $user and $password {
     file { '/etc/grub.d/01_superuser':
-      ensure  => present,
+      ensure  => file,
       mode    => '0755',
       content => "/bin/cat << EOF\nset superusers=\"${user}\"\npassword_pbkdf2 ${user} ${password}\nexport superusers\nEOF\n",
       notify  => Exec['update_grub'],
